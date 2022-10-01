953b2aaa40ff6ed00f2fa4529439417403d1a72b
PeterJames16 <peter.535210096@stu.untar.ac.id>
2022-10-01 15:28:07 +0700

    profile dan javascript

---
+++ js homepage.js
@@ -0,0 +1,180 @@
+// sidebar
+const menuItems =  document.querySelectorAll('.menu-item');
+
+// messages
+const messagesNotification = document.querySelector('#messages-notification');
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+
+// theme
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
+const fontSizes = document.querySelectorAll('.choose-size span');
+var root = document.querySelector(':root');
+const colorpalette = document.querySelectorAll('.choose-color span');
+
+
+
+
+
+// remove active class from all menu items
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+        if(item.id != 'notifications'){
+            document.querySelector('.notifications-popup').style.display = 'none';
+        }
+        else{
+            document.querySelector('.notifications-popup').style.display = 'block';
+            document.querySelector('#notifications .notification-count').style.display = 'none';
+        }
+    })
+})
+
+
+
+
+
+// buat messages
+// cari chat
+const searchMessage = () => {
+    const val = messageSearch.ariaValueMax.toLowerCase();
+    message.forEach(chat => {
+        let name = chat.querySelectorAll('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1){
+            chat.style.display = 'flex';
+        }
+        else{
+            chat.style.display = 'none';
+        }
+    })
+}
+
+// cari chat
+messageSearch.addEventListener('keyup', searchMessage);
+
+// kalo klik pesan dia bakal keluar warna
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
+})
+
+
+
+
+
+// theme display
+
+// buka modal
+const openThemeModal = () => {
+    themeModal.style.display = 'grid';
+}
+
+//closes modal
+const closeThemeModal = (e) => {
+    if(e.target.classList.contains('customize-theme')){
+        themeModal.style.display = 'none';
+    }
+}
+
+//close modal
+themeModal.addEventListener('click', closeThemeModal);
+
+
+theme.addEventListener('click', openThemeModal);
+
+
+
+// remove active
+const removeSizeSelector = () => {
+    fontSizes.forEach(size => {
+        size.classList.remove('active');
+    })
+}
+
+
+
+// font
+fontSizes.forEach(size => {
+    size.addEventListener('click', () => {
+        removeSizeSelector();
+        let fontSize;
+        size.classList.toggle('active');
+        if(size.classList.contains('font-size-1')){
+            fontSize = '10px';
+            root.style.setProperty('--sticky-top-left', '5.4rem');
+           root.style.setProperty('--sticky-top-right', '5.4rem');
+        }
+        else if(size.classList.contains('font-size-2')){
+            fontSize = '14px';
+            root.style.setProperty('--sticky-top-left', '5.4rem');
+            root.style.setProperty('--sticky-top-right', '-7rem');
+        }
+        else if(size.classList.contains('font-size-3')){
+            fontSize = '18px';
+            root.style.setProperty('--sticky-top-left', '-2rem');
+            root.style.setProperty('--sticky-top-right', '-17rem');
+        }
+        else if(size.classList.contains('font-size-4')){
+            fontSize = '22px';
+            root.style.setProperty('--sticky-top-left', '-5rem');
+            root.style.setProperty('--sticky-top-right', '-25rem');
+        }
+        else if(size.classList.contains('font-size-5')){
+            fontSize = '26px';
+            root.style.setProperty('--sticky-top-left', '-12rem');
+            root.style.setProperty('--sticky-top-right', '-35rem');
+        }
+
+        // ubah font size :root yang ada di element html
+        document.querySelector('html').style.fontSize = fontSize;
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
